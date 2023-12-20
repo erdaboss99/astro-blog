64ed1fc66f2abe1bdc090812dce7ea1201996d05
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-12-20 11:02:08 +0100

    Update README and Create constants file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import { SITE_META } from './src/lib/constants';
+
 // https://astro.build/config
 export default defineConfig({
+	prefetch: true,
+	site: SITE_META.siteUrl,
 	integrations: [tailwind()],
 });
---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const SITE_META = {
+	title: 'Erdélyi Roland | Blog',
+	headerTitle: 'Erdélyi Roland',
+	description: "Erdélyi Roland's Personal Blog",
+	language: 'en',
+	siteUrl: 'https://blog.erdelyiroland.com',
+};
