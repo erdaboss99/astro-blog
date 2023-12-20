cf378d4f44ef600c6464516fa6959d368758c365
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-12-20 13:22:11 +0100

    Add React Astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import { SITE_DATA } from './src/lib/constants';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	prefetch: true,
-	site: SITE_DATA.siteUrl,
-	integrations: [tailwind()],
+  prefetch: true,
+  site: SITE_DATA.siteUrl,
+  integrations: [tailwind(), react()]
 });
