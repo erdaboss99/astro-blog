70fd728cd2b613a436a7a2b25fd59f35c7ed7f4a
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-12-27 12:17:27 +0100

    Add Astro-MDX integration and Install TailwindCSS typography plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { SITE_DATA } from './src/lib/constants';
-
 import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: SITE_DATA.siteUrl,
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), mdx()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,38 +1,80 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
+const { fontFamily } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "media",
-  theme: {
-    fontFamily: {
-      base: ["Space Grotesk", ...fontFamily.sans],
-    },
-    colors: {
-      0: "#ffffff",
-      100: colors.gray[100],
-      200: colors.gray[200],
-      300: colors.gray[300],
-      400: colors.gray[400],
-      500: colors.gray[500],
-      600: colors.gray[600],
-      700: colors.gray[700],
-      800: colors.gray[800],
-      900: colors.gray[900],
-      950: colors.gray[950],
-      1000: "#000000",
-      transparent: "transparent",
-    },
-    extend: {
-      lineHeight: {
-        11: "2.75rem",
-        12: "3rem",
-        13: "3.25rem",
-        14: "3.5rem",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'media',
+	theme: {
+		fontFamily: {
+			base: ['Space Grotesk', ...fontFamily.sans],
+		},
+		colors: {
+			0: '#ffffff',
+			100: colors.gray[100],
+			200: colors.gray[200],
+			300: colors.gray[300],
+			400: colors.gray[400],
+			500: colors.gray[500],
+			600: colors.gray[600],
+			700: colors.gray[700],
+			800: colors.gray[800],
+			900: colors.gray[900],
+			950: colors.gray[950],
+			1000: '#000000',
+			accent: {
+				400: colors.indigo[400],
+				500: colors.indigo[500],
+				600: colors.indigo[600],
+			},
+			transparent: 'transparent',
+		},
+		extend: {
+			lineHeight: {
+				11: '2.75rem',
+				12: '3rem',
+				13: '3.25rem',
+				14: '3.5rem',
+			},
+			typography: ({ theme }) => ({
+				DEFAULT: {
+					css: {
+						a: {
+							color: theme('colors.accent.500'),
+							'&:hover': {
+								color: `${theme('colors.accent.600')}`,
+							},
+							code: { color: theme('colors.accent.400') },
+						},
+						'h1,h2': {
+							fontWeight: '700',
+							letterSpacing: theme('letterSpacing.tight'),
+						},
+						h3: {
+							fontWeight: '600',
+						},
+						code: {
+							color: theme('colors.indigo.500'),
+						},
+					},
+				},
+				invert: {
+					css: {
+						a: {
+							color: theme('colors.accent.500'),
+							'&:hover': {
+								color: `${theme('colors.accent.400')}`,
+							},
+							code: { color: theme('colors.accent.400') },
+						},
+						'h1,h2,h3,h4,h5,h6': {
+							color: theme('colors.100'),
+						},
+					},
+				},
+			}),
+		},
+	},
+	plugins: [require('@tailwindcss/typography')],
 };
